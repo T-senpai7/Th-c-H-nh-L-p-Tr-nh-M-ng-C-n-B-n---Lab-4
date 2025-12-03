032783daf6a56499642bb8d5ad799e0142d25596
T-senpai7 <94148979+T-senpai7@users.noreply.github.com>
2025-12-03 22:32:44 +0700

    Add files via upload

---
+++ Bai 3/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Bai_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.btnR = new System.Windows.Forms.Button();
+            this.DownFile = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(34, 241);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(820, 604);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btnReload);
+            this.panel1.Controls.Add(this.btnR);
+            this.panel1.Controls.Add(this.DownFile);
+            this.panel1.Controls.Add(this.btnLoad);
+            this.panel1.Location = new System.Drawing.Point(34, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(820, 170);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(788, 60);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnReload
+            // 
+            this.btnReload.Location = new System.Drawing.Point(664, 114);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(141, 44);
+            this.btnReload.TabIndex = 3;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnR
+            // 
+            this.btnR.Location = new System.Drawing.Point(409, 114);
+            this.btnR.Name = "btnR";
+            this.btnR.Size = new System.Drawing.Size(219, 44);
+            this.btnR.TabIndex = 2;
+            this.btnR.Text = "Down Resources";
+            this.btnR.UseVisualStyleBackColor = true;
+            this.btnR.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DownFile
+            // 
+            this.DownFile.Location = new System.Drawing.Point(214, 114);
+            this.DownFile.Name = "DownFile";
+            this.DownFile.Size = new System.Drawing.Size(145, 44);
+            this.DownFile.TabIndex = 1;
+            this.DownFile.Text = "Down Files";
+            this.DownFile.UseVisualStyleBackColor = true;
+            this.DownFile.Click += new System.EventHandler(this.DownFile_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(17, 113);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(145, 45);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 879);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnR;
+        private System.Windows.Forms.Button DownFile;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Bai 3/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net; // Dùng cho WebClient
+using Microsoft.Web.WebView2.Core; // Thư viện WebView2
+using HtmlAgilityPack;
+
+namespace Bai_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitWebView();
+        }
+        //khoi tao webview2
+        async void InitWebView()
+        {
+            await webView.EnsureCoreWebView2Async();
+
+        }
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          //de tai web
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            string url = textBox1.Text;
+            if (string.IsNullOrEmpty(url)) return;
+
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() != DialogResult.OK) return;
+
+            string saveFolder = folder.SelectedPath;
+
+            try
+            {
+                WebClient client = new WebClient();
+
+                
+                client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+
+                string htmlContent = client.DownloadString(url);
+
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(htmlContent);
+
+                var listImages = doc.DocumentNode.SelectNodes("//img");
+
+                if (listImages == null)
+                {
+                    MessageBox.Show("Trang web này không có ảnh nào");
+                    return;
+                }
+
+                int count = 0;
+
+                foreach (var img in listImages)
+                {
+                    string src = img.GetAttributeValue("src", "");
+
+                    if (!string.IsNullOrEmpty(src))
+                    {
+                        Uri baseUri = new Uri(url);
+                        Uri fullUri = new Uri(baseUri, src);
+
+                        string fileName = Path.GetFileName(fullUri.LocalPath);
+
+                        if (string.IsNullOrEmpty(fileName) || fileName.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
+                            fileName = "image_" + count + ".jpg";
+
+                        
+                        string savePath = Path.Combine(saveFolder, fileName);
+
+                        try
+                        {
+                            client.DownloadFile(fullUri, savePath);
+                            count++;
+                        }
+                        catch
+                        {
+                          
+                        }
+                    }
+                }
+                MessageBox.Show($"Đã tải xong {count} ảnh!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+        }
+
+
+
+        private void DownFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = textBox1.Text;
+                SaveFileDialog savefile = new SaveFileDialog();
+                savefile.Filter = "HTML File|*.html";
+                savefile.FileName = "index.html";
+                if(savefile.ShowDialog() == DialogResult.OK)
+                {
+                    WebClient client = new WebClient();
+                    client.Encoding = System.Text.Encoding.UTF8;
+                    client.DownloadFile(url, savefile.FileName);
+                    MessageBox.Show("Đã lưu xong", "Thông báo");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tải file: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webView.Reload();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            if (string.IsNullOrEmpty(url)) return;
+            try
+            {
+                webView.CoreWebView2.Navigate(url);
+                textBox1.Text = url;
+                //cap nhat lai
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi" + ex.Message);
+            }
+        }
+    }
+}
