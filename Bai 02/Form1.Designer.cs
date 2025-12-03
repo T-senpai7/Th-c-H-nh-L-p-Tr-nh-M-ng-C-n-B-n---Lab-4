8cab9d527ed912d25e64ed04445f6043c95d33b3
T-senpai7 <94148979+T-senpai7@users.noreply.github.com>
2025-12-03 22:17:35 +0700

    Add files via upload

---
+++ Bai 02/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Bai_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.url = new System.Windows.Forms.TextBox();
+            this.html = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_download = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // url
+            // 
+            this.url.BackColor = System.Drawing.Color.White;
+            this.url.Location = new System.Drawing.Point(48, 49);
+            this.url.Multiline = true;
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(673, 56);
+            this.url.TabIndex = 0;
+            this.url.TextChanged += new System.EventHandler(this.url_TextChanged);
+            // 
+            // html
+            // 
+            this.html.BackColor = System.Drawing.Color.White;
+            this.html.Location = new System.Drawing.Point(48, 151);
+            this.html.Multiline = true;
+            this.html.Name = "html";
+            this.html.Size = new System.Drawing.Size(673, 60);
+            this.html.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(19, 26);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(858, 505);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // btn_download
+            // 
+            this.btn_download.Location = new System.Drawing.Point(756, 49);
+            this.btn_download.Name = "btn_download";
+            this.btn_download.Size = new System.Drawing.Size(150, 56);
+            this.btn_download.TabIndex = 3;
+            this.btn_download.Text = "Download";
+            this.btn_download.UseVisualStyleBackColor = true;
+            this.btn_download.Click += new System.EventHandler(this.btn_download_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(29, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(891, 205);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Location = new System.Drawing.Point(29, 252);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(898, 548);
+            this.panel2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 795);
+            this.Controls.Add(this.btn_download);
+            this.Controls.Add(this.html);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.TextBox html;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_download;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Bai 02/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string getHTML(string szURL)
+        {   //tao yeu cau cho URL
+            WebRequest request = WebRequest.Create(szURL);
+            //tao phan hoi
+            WebResponse response = request.GetResponse();
+            //lay dong du lieu
+            Stream dataStream = response.GetResponseStream();
+            //Mo stream bang StreamReader doc du lieu
+            StreamReader reader = new StreamReader(dataStream);
+            //doc toan bo noi dung roi luu vao string
+            string responseFromServer = reader.ReadToEnd();
+            //dong luong roi giai phong tai nguyen
+            reader.Close();
+            response.Close();
+            return responseFromServer;
+
+
+        }
+
+        private void url_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_download_Click(object sender, EventArgs e)
+        {
+            string URL = url.Text.Trim();
+            string filePath = html.Text.Trim();
+            if (string.IsNullOrEmpty(URL))
+            {
+                MessageBox.Show("Vui lòng nhập URL");
+                return;
+
+            }
+            try
+            {
+                WebClient myClient = new WebClient();
+                myClient.DownloadFile(URL, filePath);
+                MessageBox.Show("Đã tải xong! File lưu tại: " + filePath);
+                //Dung openRead de mo luong doc du lieu
+                Stream response = myClient.OpenRead(URL);
+                //dung streamreader de doc stream va doi thanh string
+                StreamReader reader = new StreamReader(response);
+                string responseFromServer = reader.ReadToEnd();
+                richTextBox1.Text = responseFromServer;
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi" + ex.Message);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
